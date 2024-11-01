96dcac2e1c733ae3fb4a651291c003d6987e6a20
Feyerabend <set@setlonnert.com>
2024-11-01 23:51:55 +0100

    Moving

---
+++ workbook/projects/ch07/ps/ps/lexer.py
@@ -0,0 +1,10 @@
+class Token:
+    def __init__(self, type: str, value: any):
+        pass
+
+class Lexer:
+    def __init__(self, code: str):
+        pass
+
+    def tokenize(self) -> list[Token]:
+        pass
