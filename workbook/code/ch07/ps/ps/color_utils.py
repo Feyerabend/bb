9d0c795ea98dd3dd27e303753211247333e0711f
Feyerabend <set@setlonnert.com>
2024-11-05 21:29:10 +0100

    Restructured

---
+++ workbook/code/ch07/ps/ps/color_utils.py
@@ -0,0 +1,8 @@
+class ColorUtils:
+    @staticmethod
+    def rgb_to_gray(r: int, g: int, b: int) -> int:
+        pass
+
+    @staticmethod
+    def blend_colors(color1: tuple[int, int, int], color2: tuple[int, int, int], ratio: float) -> tuple[int, int, int]:
+        pass
---
+++ workbook/code/ch07/ps/ps/transformations.py
@@ -0,0 +1,13 @@
+class Transformations:
+    @staticmethod
+    def translate(matrix: list[list[float]], dx: float, dy: float) -> list[list[float]]:
+        pass
+
+    @staticmethod
+    def rotate(matrix: list[list[float]], angle: float) -> list[list[float]]:
+        pass
+
+    @staticmethod
+    def scale(matrix: list[list[float]], sx: float, sy: float) -> list[list[float]]:
+        pass
+ 
