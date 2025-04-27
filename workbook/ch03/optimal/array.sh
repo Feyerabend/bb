f71dac5bb33e3f0104d1bc66f783db72da8a11bb
Feyerabend <setlonnert@gmail.com>
2025-04-27 22:19:59 +0200

    Updates

--- workbook/ch03/optimal/array.sh
+++
@@ -1,5 +0,0 @@
-# compile with optimisations disabled
-gcc -O0 -o array array.c
-
-# compile with maximum optimisations
-gcc -O3 -o array_opt array.c
--- workbook/ch03/tools/diag/diag.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-clear
-python3 dvm.py --input sample.b > log.txt
-python3 diag.py < log.txt
