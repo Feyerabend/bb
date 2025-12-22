856b6346fdc5acc1e42218b1aebc6feae029cf0c
Feyerabend <setlonnert@gmail.com>
2025-12-22 13:35:43 +0100

    Updates

--- workbook/ch04/sec4.7/display/graphics/images/vector/vg/04/main.py
+++
@@ -1,6 +0,0 @@
-from vgf_renderer import SimpleRenderer
-from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY_2
-
-display = PicoGraphics(display=DISPLAY_PICO_DISPLAY_2)
-renderer = SimpleRenderer(display)
-renderer.render_file('image.vgf')
